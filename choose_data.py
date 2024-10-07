141b039b1a7170cf029c9cea7ee6ab8dd0c59a65
jlar0426 <nardun@gmail.com>
2024-10-07 13:54:46 +0200

    Now calculates everythin needed

--- choose_data.py
+++ choose_data.py
@@ -46,23 +46,86 @@ def read_examination(date, pid, exam):
     return exam_data, mark_data
 
 
+def calculate(data, x0, x1):
+    tdata = data.loc[(data['Time'] >= x0) & (data['Time'] <= x1)] 
+    
+    sBP_mean = np.mean(tdata['sBP'].values)
+    sBP_std = np.std(tdata['sBP'].values)
+    dBP_mean = np.mean(tdata['dBP'].values)
+    dBP_std = np.std(tdata['dBP'].values)
+    hr_mean = np.mean(tdata['HR'].values)
+    hr_std = np.std(tdata['HR'].values)
+    
+    return sBP_mean, sBP_std, dBP_mean, dBP_std, hr_mean, hr_std
 
+def plot_data(exam_data, mark_data):
+    fig, ax = plt.subplots()
+    plt.plot(exam_data['Time'], exam_data['sBP'], label = 'sBP')
+    plt.plot(exam_data['Time'], exam_data['HR'], label = 'hr')
+    plt.plot(exam_data['Time'], exam_data['dBP'], label = 'dBP')
+    
+    ax.legend()
+    rect = Rectangle((mark_data['Time'][0] + 180,max(exam_data['sBP'])), 30, 0.2, fill = False, color = 'r')
+    ax.add_patch(rect)
+    inter = stretch_rect(rect, 10)
+    
+    
+    rect2 = Rectangle((mark_data['Time'][0] - 30, min(exam_data['sBP'])), 30, 0.2, fill = False, color = 'c')
+    ax.add_patch(rect2)
+    inter2 = stretch_rect(rect2, 10)
+
+    ax.grid(True)
+    plt.title('Drag and stretch the line along the x-axis for data to include. Left mouse click to stretch, right mouse click to drag')
+    plt.show(block = True)
+    
+    mark_start = mark_data.loc[mark_data['Mark'] == 'Active standing']['Time'].values[0]
+    
+    tdata = exam_data.loc[(exam_data['Time'] >= mark_start) & 
+                          (exam_data['Time'] <= (mark_start + 20))] 
+    
+    min_sBP = min(tdata['sBP'].values)
+    min_dBP = min(tdata['dBP'].values)
+    max_HR = max(tdata['HR'].values)
+    
+    x = list(inter.get_area())
+    
+    x.sort()
+    x0 = x[0]
+    x1 = x[1]
+
+    sBP_mean, sBP_std, dBP_mean, dBP_std, hr_mean, hr_std = calculate(exam_data, x0, x1)    
+
+    print('Rect 1 sBP :')
+    print('mean: ', sBP_mean, ', std: ', sBP_std)
+    print('dBP :')
+    print('mean: ', dBP_mean, ', std: ', dBP_std)
+    print('HR :')
+    print('mean: ', hr_mean, ', std: ', hr_std)
+    print('interval: ', x0, x1)
+    
+    x_2 = list(inter2.get_area())
+    x_2.sort()
+    x2 = x_2[0]
+    x3 = x_2[1]
+    sBP_mean, sBP_std, dBP_mean, dBP_std, hr_mean, hr_std = calculate(exam_data, x2, x3)    
+
+    print('Rect 2 sBP :')
+    print('mean: ', sBP_mean, ', std: ', sBP_std)
+    print('dBP :')
+    print('mean: ', dBP_mean, ', std: ', dBP_std)
+    print('HR :')
+    print('mean: ', hr_mean, ', std: ', hr_std)
+    print('interval: ', x2, x3)
+    
+    print('minima:')
+    print('dBP: ', min_dBP, 'sBP', min_sBP)
+    print('maxima')
+    print('HR', max_HR)
 if __name__ == '__main__':
 
     date = '2019-06-13'
     pid = '020698-0931'
     
-    exam_data, mark_data= read_examination(date, pid, 'Active standing')
+    exam_data, mark_data = read_examination(date, pid, 'Active standing')
 
-    fig, ax = plt.subplots()
-    plt.plot(exam_data['Time'], exam_data['sBP'], label = 'sBP')
-    plt.plot(exam_data['Time'], exam_data['HR'], label = 'hr')
-    print(exam_data)
-    
-    ax.legend()
-    rect = Rectangle((mark_data['Time'][0] + 180,max(exam_data['sBP'])), 30, 0.2, fill = False)
-    ax.add_patch(rect)
-    inter = stretch_rect(rect, 5)
-    inter2 = interact(rect)
-
-    plt.show()
+    plot_data(exam_data, mark_data)
