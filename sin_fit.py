cc1fa02fa0382286f406c700c0f104a2924543c9
jlar0426 <nardun@gmail.com>
2024-10-07 13:54:04 +0200

    Now calculates and asks for points to be used for HR, sBP and dBP

--- sin_fit.py
+++ sin_fit.py
@@ -9,6 +9,7 @@ from scipy.fft import fft, fftfreq
 from scipy.signal import find_peaks
 import matplotlib.patches as patches
 import functools
+from matplotlib.widgets import CheckButtons, TextBox
 
 def read_examination(date, pid, resp_nr):
     """
@@ -50,7 +51,7 @@ def read_examination(date, pid, resp_nr):
 
 #    no_nan = resperation_1.drop(resperation_1[np.isnan(resperation_1.HR)].index)
 
-def fit_resp(exam):
+def fit_resp(exam, data_type):
     """
 
     Parameters
@@ -66,9 +67,10 @@ def fit_resp(exam):
 
     """
 
-    hr = exam['HR'].values
+    hr = exam[data_type].values
 
     time = exam['Time'].values
+
 
     ff = fftfreq(len(time), (time[1] - time[0]))
 
@@ -84,6 +86,9 @@ def fit_resp(exam):
 # Finds the positions of the maximas and minimas
     maximas, _ = find_peaks(hr)
     minimas, _ = find_peaks(hr*(-1))
+    
+#    print('extremas')
+#    print(minimas, maximas)
 
 # Amplitude guess based on all of the maxima and minima
     A_guess = (np.mean(hr[maximas]) - np.mean(hr[minimas]))/2
@@ -95,8 +100,19 @@ def fit_resp(exam):
 # vertical shift uses mean of the dataset as it is the indicator of the mid point of the sinus curve when np.sin() == 0
     v_guess = np.mean(hr)
 
-    print([A_guess, B_guess_fft, h_guess, v_guess])
-    param, p_cov = curve_fit(sine, time, hr, p0 = [A_guess, B_guess_fft, h_guess, v_guess])
+#    print([A_guess, B_guess_fft, h_guess, v_guess])
+#    try:
+# setting maxfev (max function evaluations) up to 10000 since I have hit situations where it failed since it took more than 1000 steps.
+    param, p_cov = curve_fit(sine, time, hr, p0 = [A_guess, B_guess_fft, h_guess, v_guess], maxfev = 10000)
+#    except:
+#        plt.scatter(time,hr)
+#        t_new = np.arange(time[0],time[-1], 0.0001)
+#        B_guess = (2*np.pi)/((time[-1]-time[0]))
+#        print(B_guess, B_guess_fft)
+#        param, p_cov = curve_fit(sine, time, hr, p0 = [A_guess, B_guess_fft, h_guess, v_guess])
+#        plt.plot(t_new, t2)
+#        plt.show()
+
 #    param2, p_cov2 = curve_fit(sine, time, hr, p0 = [A_guess, B_guess, h_guess, v_guess])
 #    print(param, param2)
     return param, p_cov, time, hr
@@ -148,10 +164,10 @@ def sine_fit(t, y, m_start, m_end):
 def set_rois(data, time):
     maximas, _ = find_peaks(data, distance = 7)
     minimas, _ = find_peaks(data*(-1), distance = 7)
-    print(maximas)
-    print(time[maximas])
-    print(minimas)
-    print(time[minimas])
+#    print(maximas)
+#    print(time[maximas])
+#    print(minimas)
+#    print(time[minimas])
     return
     
 # testing plot interacbility
@@ -239,44 +255,117 @@ class interact(object):
 
 
 def ten_period_resps(resp_data, markers, res_num):
-    print(resp_data)
-    print(markers)
+#    print(resp_data)
+#    print(markers)
     
     t_range = np.arange(markers[0], markers[1], (markers[1] - markers[0]) / 6)
     t_range = np.append(t_range,markers[1])
-    print(markers)
-    print(t_range)
+#    print(markers)
+#    print(t_range)
     
     lambd_tuplify = lambda i : (t_range[i], t_range[i+1])
     
     time_defs = list(map(lambd_tuplify, range(len(t_range) -1)))
-    
+
+# Splits a repsiration period into 6 closely equal size periods.
     def split_data(interval):
         return resp_data.loc[(resp_data['Time'] > (interval[0] - 0.5)) & 
                              (resp_data['Time'] < (interval[1] + 0.5))]
 
-    test = list(map(split_data, time_defs))
-    print(test[0])
+    periods = list(map(split_data, time_defs))
 
-    fig, ax = plt.subplots()
+    hr_data, chosen = image_handling(periods, res_num, 'HR')
+
+    
+    mean_hr, std_hr, maxs_hr, mins_hr = data_handling(hr_data, chosen) 
+    bp_data, chosen1, chosen2 = image_handling(periods, res_num, ['sBP', 'dBP'])
+    
+    sBP_data = [x[0] for x in bp_data]
+    dBP_data = [x[1] for x in bp_data]
+
+    mean_sBP, std_sBP, maxs_sBP, mins_sBP = data_handling(sBP_data, chosen1)
+    mean_dBP, std_dBP, maxs_dBP, mins_dBP = data_handling(dBP_data, chosen2)
+
+    hr = [mean_hr, std_hr, maxs_hr, mins_hr, chosen]
+    sBP = [mean_sBP, std_sBP, maxs_sBP, mins_sBP, chosen1]
+    dBP = [mean_dBP, std_dBP, maxs_dBP, mins_dBP, chosen2]
+
+    return hr, sBP, dBP
+
+def image_handling(periods, res_num, used_data):
+    fig, ax = plt.subplots(height_ratios = [0.1])
     ax.grid(True)
-
-    for i in range(len(test)):
-        param, p_cov, time, hr = fit_resp(test[i])
-        print(time)
-        t2 = np.arange(time[0],time[-1], 0.1)
-        t = sine(t2, param[0], param[1], param[2], param[3])
-
-        ax.plot(t2, t, label = 'sine fit fourier')
-        ax.scatter(time, hr, label = 'sBP')
 
     tit = 'respiration %d' % (res_num+1)
     plt.title(tit)
-    ax.legend()
-    plt.show()    
-    
-    return
 
+    data = []
+    for i in range(len(periods)):
+        if type(used_data) == str:
+            param, p_cov, time, d_type = fit_resp(periods[i], used_data)
+
+            data.append(d_type)
+            t2 = np.arange(time[0],time[-1], 0.1)
+            t = sine(t2, param[0], param[1], param[2], param[3])            
+
+            ax.plot(t2, t, label = 'line' + str(i))
+            ax.scatter(time, d_type)
+            fig.text(0.30, 0.22, 'hr')
+
+        elif type(used_data) == list:
+            param1, p_cov1, time1, d_type1 = fit_resp(periods[i], used_data[0])
+            param2, p_cov2, time2, d_type2 = fit_resp(periods[i], used_data[1])
+
+            data.append([d_type1, d_type2])
+            time2 = np.arange(time1[0],time1[-1], 0.1)
+            
+            t1 = sine(time2, param1[0], param1[1], param1[2], param1[3])            
+            t2 = sine(time2, param2[0], param2[1], param2[2], param2[3])
+            
+            ax.plot(time2, t1, label = 'line' + str(i) + ', ' + used_data[0])
+            ax.scatter(time1, d_type1)
+            ax.plot(time2, t2, label = 'line' + str(i) + ', ' + used_data[1])
+            ax.scatter(time1, d_type2)
+
+            cb_pos2 = plt.axes([0.50, 0.05, 0.15, 0.15])
+            check_but2 = CheckButtons(cb_pos2, list(range(6)), actives = [True, True, True, True, True, True])
+            fig.text(0.50, 0.22, 'dBP')
+            fig.text(0.30, 0.22, 'sBP')
+
+
+    ax.legend()
+    ax.set_position([0.1,0.30, 0.85, 0.65])
+
+    cb_pos = plt.axes([0.30, 0.05, 0.15, 0.15])
+    fig.text(0.15, 0.15, 'Which needs to be used?')
+    check_but = CheckButtons(cb_pos, list(range(6)), actives = [True, True, True, True, True, True])
+
+    plt.show(block = True)
+    
+    chosen = check_but.get_status()
+    if type(used_data) == list:
+        chosen2 = check_but2.get_status()
+        return data, chosen, chosen2
+    return data, chosen
+
+
+def data_handling(data, chosen):
+    used_data = np.array([])
+    maxs = []
+    mins = []
+    for i in range(len(chosen)):
+        d = data[i] * chosen[i]
+        maxima = max(d)
+        minima = min(d)
+        used_data = np.append(used_data, [maxima - minima])
+        maxs.append(maxima)
+        mins.append(minima)
+    used_data = [d for d in used_data if d != 0]
+    print(used_data)    
+    mean = np.mean(used_data)
+    std = np.std(used_data)
+    return mean, std, maxs, mins
+    
 if __name__ == '__main__':
 
     date = '2019-06-13'
@@ -284,31 +373,16 @@ if __name__ == '__main__':
     
     resp_datas, resp_times = read_examination(date, pid, 0)
     for i in range(3):
-        param, p_cov, time, hr = fit_resp(resp_datas[i])
-        ten_period_resps(resp_datas[i], resp_times[i], i)
-
-        t2 = np.arange(time[0],time[-1], 0.1)
-        t = sine(t2, param[0], param[1], param[2], param[3])
-    #t = param[0] * np.sin(param[1]*time+param[2])+param[3]
-#    t2 = param2[0] * np.sin(param2[1]*time+param2[2])+param2[3]
-
-        fig, ax = plt.subplots()
-        tit = 'respiration %d' % (i+1)
-        plt.title(tit)
-        ax.plot(t2, t, label = 'sine fit fourier')
-#    ax.scatter(time, hr, label = 'heart rate')
-        ax.scatter(time, hr, label = 'sBP')
-#    ax.plot(time, t2, label = 'sine fit 2')
-        ax.legend()
-        ax.grid(True)
+#        param, p_cov, time, hr = fit_resp(resp_datas[i])
+        hr, sBP, dBP = ten_period_resps(resp_datas[i], resp_times[i], i)
+        print('respiration ' + str(i+1))
+        print('hr')
+        print(hr)
+        print('sBP')
+        print(sBP)
+        print('dBP')
+        print(dBP)
 
 
     t = patches.Rectangle((700,60), 20, 20, fill = False)
     rect = patches.Rectangle((650,60), 20, 20, fill = False)
-#    ax.add_patch(t)
-#    ax.add_patch(rect)
-#    inter = interact([t,rect])
-# I will be able to create squares for indicating areas where my program will look for data using patches.Rectangle
-    
-#    plt.ion()
-#    plt.show()
